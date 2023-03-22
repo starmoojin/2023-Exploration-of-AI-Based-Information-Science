cf937e895321cffcd1897d06461407c3c0fba26e
starmoojin <starmoojin@gmail.com>
2023-03-22 20:43:26 +0900

    week03 prime number checker v.03

--- primenumber.py
+++ primenumber.py
@@ -4,8 +4,7 @@ is_prime = True
 for i in range(2, number):
     if number % i == 0:
         is_prime = False
-
-if is_prime == 0:
+if is_prime == True:
     print(f'{number} is prime number.')
 else:
     print(f'{number} is not prime number.')

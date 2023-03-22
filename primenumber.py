cc2c2fcf822483d9b0765d66ee2e9e86c5cba002
starmoojin <starmoojin@gmail.com>
2023-03-22 20:52:56 +0900

    week03 prime number checker v.06

--- primenumber.py
+++ primenumber.py
@@ -1,13 +1,16 @@
 number = int(input("Input number: "))
 is_prime = True
 
-for i in range(2, number):
-    if number % i == 0:
-        is_prime = False
-        break # If not prime number, exit the loop as soon as the first divisor is found
-    print(i, end=' ')
-#if is_prime == True:
-if is_prime: # remove comparison
-    print(f'{number} is prime number.')
+if number < 2:
+    is_prime = False
 else:
-    print(f'{number} is not prime number.')
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+            break
+        print(i, end=' ')
+if is_prime:
+        print(f'{number} is prime number.')
+else:
+        print(f'{number} is not prime number.')
+

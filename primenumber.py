1f0612ddf19c74a332c16e6c6711f1f3d4c853ca
starmoojin <starmoojin@gmail.com>
2023-03-22 20:39:46 +0900

    week03 prime number checker v.02

--- primenumber.py
+++ primenumber.py
@@ -1,11 +1,11 @@
 number = int(input("Input number: "))
-count = 0
+is_prime = True
 
 for i in range(2, number):
     if number % i == 0:
-        count = count + 1
+        is_prime = False
 
-if count == 0:
+if is_prime == 0:
     print(f'{number} is prime number.')
 else:
     print(f'{number} is not prime number.')
